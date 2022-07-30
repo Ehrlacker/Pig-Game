0c6a2bbc15c92d1bf574e2547a0f9cc2ae1da8e1
Ehrlacker <101287793+Ehrlacker@users.noreply.github.com>
2022-07-30 18:52:44 -0400

    Remove dice pic when games over.

--- script.js
+++ script.js
@@ -27,77 +27,89 @@ let currentScore = 0;
 //^Is a let so we can keep updating it.
 let activePlayer = 0;
 //player 1 is 0
+let playing = true;
+//This^ is to reset the game when the score limit is reached.
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   //^the class you want, .addEventListener, then the event you want,
   //then the function.
-  //1. Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    //^As lon as playing is true, execute this code.
+    //Since playing is a boolean, we don't have to write if(playing === true).
+    //1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  //2.Display dice
-  diceEl.classList.remove('hidden');
-  //3.Check for a rolled 1: and if true, switch to next player.
-  diceEl.src = `dice-${dice}.png`;
-  if (dice !== 1) {
-    //Add dice to current score
-    currentScore = currentScore + dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    //^This = current score goes to whatever player is active.
-  } else {
-    //switch to next player
+    //2.Display dice
+    diceEl.classList.remove('hidden');
+    //3.Check for a rolled 1: and if true, switch to next player.
+    diceEl.src = `dice-${dice}.png`;
+    if (dice !== 1) {
+      //Add dice to current score
+      currentScore = currentScore + dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      //^This = current score goes to whatever player is active.
+    } else {
+      //switch to next player
 
-    //dice = random number variable
-    //^This is saying whatever random # is chosen is the
-    // dice # that will appear.
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    //^We're selecting the current player which at this point is 0
-    //Then we set their textContent to 0, then we switch
-    //from player 0 to player 1
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    //^If active player is 0, then we want the new
-    //active player to be 1, else, it should be 0.
-    //If player rolls a 1,, their score goes to 0.
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+      //dice = random number variable
+      //^This is saying whatever random # is chosen is the
+      // dice # that will appear.
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+      //^We're selecting the current player which at this point is 0
+      //Then we set their textContent to 0, then we switch
+      //from player 0 to player 1
+      currentScore = 0;
+      activePlayer = activePlayer === 0 ? 1 : 0;
+      //^If active player is 0, then we want the new
+      //active player to be 1, else, it should be 0.
+      //If player rolls a 1,, their score goes to 0.
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player--active');
 
-    //^Toggle adds the class if it isn't there,
-    //if it is there, it will remove the class.
+      //^Toggle adds the class if it isn't there,
+      //if it is there, it will remove the class.
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
-  //1)Add current score to active player's score
-  scores[activePlayer] += currentScore;
-  console.log(scores[activePlayer]);
+  if (playing) {
+    //1)Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    console.log(scores[activePlayer]);
 
-  //^When player is 1, it will be scores 1
-  //When player is 0, it will be score 0
-  //Then we take that value and add that current score to it.
-  //and assign it to scores at current active player.
-  //ex... scores[1] = scores[1] + currentScore
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
-  //2)Check if score is already at least 100
-  //If so, finish the game,
-  //If not, switch to the next player.
-  if (scores[activePlayer] >= 100) {
-    //Finish game
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player--winner');
+    //^When player is 1, it will be scores 1
+    //When player is 0, it will be score 0
+    //Then we take that value and add that current score to it.
+    //and assign it to scores at current active player.
+    //ex... scores[1] = scores[1] + currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    //2)Check if score is already at least 100
+    //If so, finish the game,
+    //If not, switch to the next player.
+    if (scores[activePlayer] >= 10) {
+      //Finish game
+      playing = false;
+      diceEl.classList.add('hidden');
+      //^This removes the dice picture when the game is over.
 
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove('player--active');
-  } else {
-    //Switch to next player.
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      //Switch to next player.
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+      currentScore = 0;
+      activePlayer = activePlayer === 0 ? 1 : 0;
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player--active');
+    }
   }
 });
